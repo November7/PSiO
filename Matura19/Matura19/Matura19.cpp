1e2d588aed9a2d888f8c7b1bf401a33d99f21ad3
November7 <m.kowalski@zsme.tarnow.pl>
2019-06-02 23:46:47 +0200

    Matura 2019 - init

---
+++ Matura19/Matura19/Matura19.cpp
@@ -0,0 +1,12 @@
+﻿// Matura19.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Zadanie 1.1" << endl;
+}
